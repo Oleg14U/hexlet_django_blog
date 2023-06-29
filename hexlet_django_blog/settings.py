aec84e39715fbbe09e3639fb8667912315b06c54
Oleg14U <hohleghek@yandex.ru>
2023-06-29 16:09:02 +0300

    App

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hexlet_django_blog',
-]
+    'hexlet_django_blog.article',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
