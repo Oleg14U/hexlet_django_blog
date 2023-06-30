03378e865c1dd57e4f3c4c14db5376e9251c5250
Oleg14U <hohleghek@yandex.ru>
2023-06-30 14:29:15 +0300

    Extend

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+import datetime
 
 def index(request):
-    return HttpResponse('article')
+    title = 'Заголовок'
+    description = 'Описание'
+    created_at = datetime.date.today
+    return render(request, 'articles/index.html', context={
+        'title': title, 'description': description,
+        'created_at': created_at,
+    })
